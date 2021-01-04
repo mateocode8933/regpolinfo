6429a7259be9282c6b7c182a5f6d7d1889087a97
Mateusz Skrobiś <mateocode9833@gmail.com>
2021-01-04 22:06:11 +0100

    commit test

--- src/components/Header.js
+++ src/components/Header.js
@@ -170,7 +170,7 @@ const Navbar = () => {
         </div>
         <div className={stylesCss.openingHours}>
           Godziny Otwarcia <br />
-          Poniedziałek-Piątek: 8:00 - 16:20 lol
+          Poniedziałek-Piątek: 8:00 - heheszki
         </div>
         {burger ? (
           <TestNav>
