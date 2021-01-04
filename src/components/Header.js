0c7c664d889b432b9be9b036d5e191565a113efd
Mateusz Skrobiś <mateocode9833@gmail.com>
2021-01-04 21:55:42 +0100

    connected netlify

--- src/components/Header.js
+++ src/components/Header.js
@@ -170,7 +170,7 @@ const Navbar = () => {
         </div>
         <div className={stylesCss.openingHours}>
           Godziny Otwarcia <br />
-          Poniedziałek-Piątek: 8:00 - 16:00
+          Poniedziałek-Piątek: 8:00 - 16:20
         </div>
         {burger ? (
           <TestNav>
